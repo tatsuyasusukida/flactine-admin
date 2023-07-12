506e0de0189c8d8789c3f2e6ba046972c8e478df
susukida <susukida@loremipsum.co.jp>
2023-07-12 18:31:13 +0900

    お客さま情報のエクセル入力のバリデーションを実装中です。
    ここまでの作業時間は 3.5 時間くらいです。

--- src/pages/api/customers/import/validate.ts
+++ src/pages/api/customers/import/validate.ts
@@ -19,16 +19,156 @@ export default async function handler(
 
   const buffer = Buffer.concat(chunks);
   const workbook = xlsx.read(buffer);
+  const sheetName = "Sheet 1";
+  const sheet = workbook.Sheets[sheetName];
 
-  console.log(workbook.Sheets["Sheet 1"]);
+  if (!sheet) {
+    res.send({
+      ok: false,
+      errorMessages: [`${sheetName} が見つかりません。`],
+    });
+
+    return;
+  }
+
+  const rows: { [headerCell: string]: string }[] =
+    xlsx.utils.sheet_to_json(sheet);
+
+  if (rows.length < 1) {
+    res.send({
+      ok: false,
+      errorMessages: ["行数が 0 件です。"],
+    });
+
+    return;
+  }
+
+  const actualHeaderCells = Object.keys(rows[0]);
+  const expectedHeaderCells = [
+    "番号",
+    "顧客名",
+    "LINE ID",
+    "配達スタッフ",
+    "固定電話",
+    "携帯電話",
+  ];
+
+  const notFoundHeaderCells = expectedHeaderCells.filter((headerCell) => {
+    return actualHeaderCells.indexOf(headerCell) === -1;
+  });
+
+  if (notFoundHeaderCells.length >= 1) {
+    res.send({
+      ok: false,
+      errorMessages: notFoundHeaderCells.map((headerCell) => {
+        return `${headerCell}列が見つかりません。`;
+      }),
+    });
+
+    return;
+  }
+
+  const monthHeaderPattern = /^(\d{4})年(\d{1,2})月$/;
+  const deliveryHeaderCells: {
+    text: string;
+    year: number;
+    month: number;
+  }[] = [];
+
+  actualHeaderCells.forEach((text) => {
+    const match = text.match(monthHeaderPattern);
+
+    if (match) {
+      const year = parseInt(match[1], 10);
+      const month = parseInt(match[2], 10);
+
+      deliveryHeaderCells.push({ text, year, month });
+    }
+  });
+
+  if (deliveryHeaderCells.length < 1) {
+    res.send({
+      ok: false,
+      errorMessages: ["月別配達日列が見つかりません。"],
+    });
+
+    return;
+  }
+
+  const invalidDeliveryHeaderCells = deliveryHeaderCells.filter(
+    (deliveryHeaderCell) => {
+      const { month } = deliveryHeaderCell;
+      return month < 1 || 12 < month;
+    }
+  );
+
+  if (invalidDeliveryHeaderCells.length >= 1) {
+    res.send({
+      ok: false,
+      errorMessages: invalidDeliveryHeaderCells.map((deliveryHeaderCell) => {
+        return `${deliveryHeaderCell.text}列の年月が不正です。`;
+      }),
+    });
+
+    return;
+  }
+
+  const customers = rows.map((row) => {
+    const number = row["番号"];
+    const name = row["顧客名"];
+    const lineUserId = row["LINE ID"];
+    const deliveryStaff = row["配達スタッフ"];
+    const tel = row["固定電話"];
+    const mobile = row["携帯電話"];
+    const deliveries = deliveryHeaderCells.map(({ text, year, month }) => {
+      const input = row[text];
+      return { year, month, input };
+    });
+
+    return {
+      number,
+      name,
+      lineUserId,
+      deliveryStaff,
+      tel,
+      mobile,
+      deliveries,
+    };
+  });
+
+  const errorMessages: string[] = [];
+
+  customers.forEach((customer, i) => {
+    const rowNumber = i + 2;
+    const number = parseInt(customer.number, 10);
+
+    if (isNaN(number) || number <= 0) {
+      errorMessages.push(
+        `${rowNumber} 行目：番号を 1 以上の整数でご入力ください。`
+      );
+    }
+
+    if (customer.name === "" || customer.name.length > 100) {
+      errorMessages.push(
+        `${rowNumber} 行目：顧客名を 1〜100 文字でご入力ください。`
+      );
+    }
+
+    if (
+      customer.lineUserId !== "" &&
+      /^[0-9A-Za-z]{33}$/.test(customer.lineUserId)
+    ) {
+      if (customer.lineUserId === "" || customer.lineUserId.length > 100) {
+        errorMessages.push(
+          `${rowNumber} 行目：LINE ID を 33 文字の半角英数字でご入力ください。`
+        );
+      }
+    }
+  });
 
   res.send({
-    ok: false,
-    errorMessages: [
-      "ここにエラーメッセージが入ります。",
-      "ここにエラーメッセージが入ります。",
-      "ここにエラーメッセージが入ります。",
-    ],
+    ok: errorMessages.length === 0,
+    errorMessages: [],
   });
 }
 
