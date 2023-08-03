33647ce5012d01a6e6897f7d6cc8fadaa1bb76ab
susukida <susukida@loremipsum.co.jp>
2023-08-04 08:14:54 +0900

    LineMessage のシードを追加しました。

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Delivery, PrismaClient } from '@prisma/client';
+import { Delivery, LineMessage, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -6,11 +6,11 @@ async function main() {
   const user = {
     id: 1,
     number: 1,
-    name: '名古屋 花子',
-    lineUserId: 'U00000000000000000000000000000000',
-    deliveryStaff: '関',
-    tel: '05212345678',
-    mobile: '09012345678',
+    name: "名古屋 花子",
+    lineUserId: "U00000000000000000000000000000000",
+    deliveryStaff: "関",
+    tel: "05212345678",
+    mobile: "09012345678",
   };
 
   await prisma.user.upsert({
@@ -19,34 +19,34 @@ async function main() {
     create: user,
   });
 
-  const deliveries: Omit<Delivery, 'createdAt' | 'updatedAt'>[] = [
+  const deliveries: Omit<Delivery, "createdAt" | "updatedAt">[] = [
     {
       id: 1,
-      date: new Date('2023-07-03'),
+      date: new Date("2023-07-03"),
       skip: false,
       userId: user.id,
     },
     {
       id: 2,
-      date: new Date('2023-07-17'),
+      date: new Date("2023-07-17"),
       skip: true,
       userId: user.id,
     },
     {
       id: 3,
-      date: new Date('2023-07-31'),
+      date: new Date("2023-07-31"),
       skip: false,
       userId: user.id,
     },
     {
       id: 4,
-      date: new Date('2023-08-14'),
+      date: new Date("2023-08-14"),
       skip: false,
       userId: user.id,
     },
     {
       id: 5,
-      date: new Date('2023-08-28'),
+      date: new Date("2023-08-28"),
       skip: false,
       userId: user.id,
     },
@@ -57,6 +57,28 @@ async function main() {
       where: { id: delivery.id },
       update: delivery,
       create: delivery,
+    });
+  }
+
+  const lineMessages: Omit<LineMessage, "createdAt" | "updatedAt">[] = [
+    ...Array(5),
+  ].map((_, i) => ({
+    id: i + 1,
+    date: new Date(2023, 6 - 1, 20, 12, 34, 56),
+    lineUserName: "山田　花子",
+    lineUserId: "U00000000000000000000000000000000",
+    content: "\nここに本文が入ります".repeat(5).slice(1),
+    isSent: true,
+    errorCount: 0,
+    errorMessage: "",
+    errorStack: "",
+  }));
+
+  for (const lineMessage of lineMessages) {
+    await prisma.lineMessage.upsert({
+      where: { id: lineMessage.id },
+      update: lineMessage,
+      create: lineMessage,
     });
   }
 }
