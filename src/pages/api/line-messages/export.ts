15b11ab0d929ac68b9885815017eabd4ca21738b
susukida <susukida@loremipsum.co.jp>
2023-08-04 08:23:26 +0900

    LINE メッセージの送信履歴のダウンロードを実装しました。

--- src/pages/api/line-messages/export.ts
+++ src/pages/api/line-messages/export.ts
@@ -6,43 +6,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // const users = await prisma.user.findMany({
-  //   orderBy: {
-  //     number: "asc",
-  //   },
-  //   include: {
-  //     deliveries: {
-  //       orderBy: {
-  //         date: "asc",
-  //       },
-  //     },
-  //   },
-  // });
-
-  // const months: string[] = [];
-
-  // for (const user of users) {
-  //   for (const delivery of user.deliveries) {
-  //     const year = delivery.date.getUTCFullYear();
-  //     const month = delivery.date.getUTCMonth() + 1;
-  //     const yearMonth = [
-  //       year.toString().padStart(4, "0"),
-  //       month.toString().padStart(2, "0"),
-  //     ].join("-");
-
-  //     if (months.indexOf(yearMonth) === -1) {
-  //       months.push(yearMonth);
-  //     }
-  //   }
-  // }
-
-  // const sortedMonths = months.sort().map((yearMonth) => {
-  //   const [yearStr, monthStr] = yearMonth.split("-");
-  //   const year = parseInt(yearStr, 10);
-  //   const month = parseInt(monthStr, 10);
-
-  //   return { year, month };
-  // });
+  const lineMessages = await prisma.lineMessage.findMany({
+    orderBy: {
+      date: "desc",
+    },
+  });
 
   const headerCells = [
     "送信日時",
@@ -53,65 +21,24 @@ export default async function handler(
     "エラーメッセージ",
   ];
 
-  const rows = [...Array(10)].map((_) => [
-    "2023年6月20日 (火) 12時34分56秒",
-    "山田　花子",
-    "Ue3f16543e8e6ea4553d351fa36ae8c76",
-    [
-      "ここに本文が入ります。",
-      "ここに本文が入ります。",
-      "ここに本文が入ります。",
-      "ここに本文が入ります。",
-      "ここに本文が入ります。",
-    ].join("\n"),
-    "成功",
-    "",
-  ]);
+  const rows = lineMessages.map((lineMessage) => {
+    const statusText = lineMessage.isSent
+      ? "成功"
+      : lineMessage.errorCount >= 1
+      ? lineMessage.errorMessage
+      : "未送信";
 
-  // const rows = users.map((user) => {
-  //   return [
-  //     user.number.toString(),
-  //     user.name,
-  //     user.lineUserId,
-  //     user.deliveryStaff,
-  //     user.tel,
-  //     user.mobile,
-  //     ...sortedMonths.map(({ year, month }) => {
-  //       const filteredDeliveries = user.deliveries.filter((delivery) => {
-  //         const deliveryYear = delivery.date.getUTCFullYear();
-  //         const deliveryMonth = delivery.date.getUTCMonth() + 1;
+    const errorMessage = lineMessage.isSent ? "" : lineMessage.errorMessage;
 
-  //         return year === deliveryYear && month === deliveryMonth;
-  //       });
-
-  //       const deliveryGroups = [...Array(7)].map((_, dayOfWeek) => {
-  //         const deliveries = filteredDeliveries.filter((delivery) => {
-  //           return delivery.date.getUTCDay() === dayOfWeek;
-  //         });
-
-  //         return { dayOfWeek, deliveries };
-  //       });
-
-  //       return deliveryGroups
-  //         .filter(({ deliveries }) => {
-  //           return deliveries.length >= 1;
-  //         })
-  //         .map(({ dayOfWeek, deliveries }) => {
-  //           return (
-  //             deliveries
-  //               .map((delivery) => {
-  //                 const dayOfMonth =
-  //                   Math.floor((delivery.date.getUTCDate() - 1) / 7) + 1;
-  //                 const skip = delivery.skip ? "休" : "";
-  //                 return `${dayOfMonth}${skip}`;
-  //               })
-  //               .join("・") + "日月火水木金土"[dayOfWeek]
-  //           );
-  //         })
-  //         .join(" ");
-  //     }),
-  //   ];
-  // });
+    return [
+      convertDate(lineMessage.date),
+      lineMessage.lineUserName,
+      lineMessage.lineUserId,
+      lineMessage.content,
+      statusText,
+      errorMessage,
+    ];
+  });
 
   const workbook = xlsx.utils.book_new();
   const worksheet = xlsx.utils.aoa_to_sheet([headerCells, ...rows]);
@@ -165,4 +92,16 @@ export default async function handler(
   );
 
   res.send(buffer);
+}
+
+function convertDate(date: Date): string {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const dayMonth = date.getDate();
+  const dayWeek = "日月火水木金土"[date.getDay()];
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  return `${year}年${month}月${dayMonth}日 (${dayWeek}) ${hour}時${minute}分${second}秒`;
 }
